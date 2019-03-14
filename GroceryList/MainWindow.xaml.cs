34ff7c7315a6ebcdc4cead6e7ae645a5f13cc798
austin bangert <austinbangert3@gmail.com>
2019-03-14 11:53:15 -0400

    initial commit

---
+++ GroceryList/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using GroceryList.Views;
+using MahApps.Metro.Controls;
+
+namespace GroceryList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        GroceryListView _groceryListView = new GroceryListView();
+        RecipeView _recipeView = new RecipeView();
+        IngredientsView _ingredientsView = new IngredientsView();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainContent.Content = _groceryListView;
+        }
+
+        private void GroceryListButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = _groceryListView;
+        }
+
+        private void RecipesButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = _recipeView;
+        }
+
+        private void IngredientsButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = _ingredientsView;
+        }
+    }
+}
